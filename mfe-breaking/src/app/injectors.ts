65df6885fa4e18b2808a14de60de5ea8186211d7
Riccardo Turri <riccardo.turri.ext@siemens.com>
2023-10-17 11:17:19 +0200

    remove proj

--- mfe-breaking/src/app/injectors.ts
+++
@@ -1,4 +0,0 @@
-import { InjectionToken } from '@angular/core';
-import { RemoteTemplateConfiguration } from './remote-template/models/remote-template-configuration.model';
-
-export const REMOTE_TEMPLATE_CONFIGURATION = new InjectionToken<RemoteTemplateConfiguration>('REMOTE_TEMPLATE_CONFIGURATION');
